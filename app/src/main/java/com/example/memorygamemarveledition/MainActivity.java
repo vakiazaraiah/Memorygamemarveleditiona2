2c55f72c7f0df437b3beda02a5040bfa6acf2ba2
Azaraiah Vaki <jc319582@ad.jcu.edu.au>
2019-05-23 21:06:41 +1000

    hello

---
+++ app/src/main/java/com/example/memorygamemarveledition/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.memorygamemarveledition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Stopped at 4:55sec
+    }
+}
